f05233a29b6012fd8b47dae17cf462e6c30d9ca9
Brian Kramer <subsoap@gmail.com>
2018-02-21 11:09:32 -0800

    More progress

--- xp/xp.lua
+++ xp/xp.lua
@@ -23,4 +23,16 @@ end
 function M.add_xp(id, amount)
 end
 
+-- Percent is 0-1
+function M.scale_gui_bar_clipper_size_x(node, percent, original_size)
+	assert(percent <= 1 and percent >= 0, "XP: scale_gui_bar_clipper_size_x requires a percent in the range of 0-1")
+	percent = math.max(0, percent)
+	percent = math.min(100, percent)
+	gui.set_size(node, vmath.vector3(original_size.x * percent, original_size.y, original_size.z))	
+end
+
+function M.scale_gui_bar_clipper_size_y(node, percent, original_size)
+	M.scale_gui_bar_clipper_size_x(node, percent, original_size)
+end
+	
 return M
